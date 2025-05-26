3c901dbc0ccd7a99ab6360aa669b7281bcc3d12f
rsyarsya <rasyadwaarsyairnantyanto2006@mail.ugm.ac.id>
2025-05-26 12:17:37 +0700

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -17,9 +17,8 @@ const nextConfig = {
       {
         source: "/api/:path*",
         destination: process.env.NEXT_PUBLIC_API_URL
-          ? `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`
-          : "https://v0-pneu-scope-production.up.railway.app/api/:path*",
-      },
+        ? `${process.env.NEXT_PUBLIC_API_URL}/:path*`  // Hapus /api di sini
+        : "https://v0-pneu-scope-production.up.railway.app/:path*",
       {
         source: "/ml/:path*",
         destination: process.env.NEXT_PUBLIC_ML_SERVICE_URL
